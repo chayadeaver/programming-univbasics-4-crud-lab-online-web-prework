d3cdc6bec4c435811f6bc3ca3eeec8d149bcda96
Chaya Deaver <chayadeaver@gmail.com>
2019-07-31 17:58:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "item2", "item3", "item4"]
+  array = ["wow", "I", "am", "learning"]
+  array.push["arrays"]
   
 end
 
