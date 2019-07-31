7240b581e0a057ce288fbc79ccb0a8365dd56ffb
Chaya Deaver <chayadeaver@gmail.com>
2019-07-31 18:49:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.push
+  array.push()
 end
 
 def remove_element_from_start_of_array(array)
